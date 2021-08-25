b5e1181a077de37e034c54164c695207a629beb5
天衡 <13265317640@163.com>
2021-08-25 18:10:40 +0800

    style: 删除不必要的测试项目

--- packages/tingshu/index.ts
+++
@@ -1,9 +0,0 @@
-import { createApp, inject, provide, emit, on, Get, Post } from "@qy-mock/core";
-const { app, mockApp } = createApp("apiV3/audio2");
-import { AdminRouter } from "./src/router";
-mockApp
-  .registerRouter([AdminRouter])
-  .setBaseResponse({
-    customTransformer: (data) => Object.assign(data, { resultCode: 1 }),
-  })
-  .mount("40003");
